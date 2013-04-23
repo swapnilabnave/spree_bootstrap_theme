2472025f15ed11eab495354824dbd5b90c33d083
Swapnil Abnave <swapnil.abnave@kiprosh.com>
2013-04-23 18:09:30 +0530

    remove body deface

--- app/overrides/replace_body.rb
+++ app/overrides/replace_body.rb
@@ -1,5 +1,1 @@
-Deface::Override.new(:virtual_path => %q{spree/layouts/spree_application},
-                     :name => %q{replace_body},
-                     :replace => %q{[data-hook='body']},
-                     :disabled => false,
-                     :partial => %q{spree/overrides/replace_body})
+
